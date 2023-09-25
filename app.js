88dc58b03a2cd054d19f39bf3697b5db8833344c
William <William.wibeck@gmail.com>
2023-09-26 00:33:48 +0200

    First commit, base structure of portfolio.

---
+++ app.js
@@ -0,0 +1,10 @@
+const navLinks = document.querySelectorAll('.contentList');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const targetId = link.getAttribute('href');
+    const targetSection = document.querySelector(targetId);
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+  });
+});
